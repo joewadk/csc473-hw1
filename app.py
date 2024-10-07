38c0cfee07eada12def929bd4251b3f543f026a7
joewadk <jkabir000@citymail.cuny.edu>
2024-10-07 00:46:39 -0400

    fixed restaurant name param and added comments. should be done i think

--- app.py
+++ app.py
@@ -5,8 +5,9 @@ from inspector import Inspector
 app = Flask(__name__)
 
 
-@app.route("/search", methods=["GET"])
+@app.route("/search", methods=["GET"]) #search route, using get to get an output
 def search():
+    
     '''
     Below you will find several examples of requests that clients might make:
   - `curl --location --request GET 'http://127.0.0.1:8080/search?restaurant_name=pizza'`
@@ -21,26 +22,33 @@ def search():
     - [Example response](golden_files/search_by_restaurant_name_cuisine_and_zipcode.json)
     ''' #adding here for self-reference
 
+
+
     #create inspector object
     #load json
     #search for any args
 
+
+
     restaurant=request.args.get('restaurant_name')
     cuisine=request.args.get('cuisine')
     zipcode=request.args.get('zipcode')
-    limit= request.args.get('limit', default = 10, type = int)
-    print(restaurant, cuisine, zipcode)
-    inspections = [inspection.to_json() for inspection in Inspector.get_inspections()]
-    if restaurant:
-        inspections = [insp for insp in inspections if insp['restaurant_name'].lower() == restaurant.lower()]
+    limit= request.args.get('limit', default = 10, type = int) #default limit is 10, but can be changed by user
+    print(restaurant, cuisine, zipcode) #debug to see if i get the right params
+    inspections = [inspection.to_json() for inspection in Inspector.get_inspections()] #load the json
+
+
+    #conditional statements to filter the inspections
+    if restaurant: 
+        inspections = [insp for insp in inspections if restaurant.lower() in insp['restaurant_name'].lower() ] #case insensitive, and also checks if param inside restaurant_name
     if cuisine:
-        inspections = [insp for insp in inspections if insp['cuisine'].lower() == cuisine.lower()]
+        inspections = [insp for insp in inspections if insp['cuisine'].lower() == cuisine.lower()]#case insensitive, and also checks if param == cuisine
     if zipcode:
-        inspections = [insp for insp in inspections if insp['zipcode'] == zipcode]
-    inspections.sort(key=lambda x: x['restaurant_id'])
-    return jsonify(inspections[:limit])
+        inspections = [insp for insp in inspections if insp['zipcode'] == zipcode] #strictly checks if param == zipcode
+    inspections.sort(key=lambda x: x['restaurant_id']) #lambda to sort by restaurant_id
+    return jsonify(inspections[:limit]) #return the jsonified inspections with the limit
 
 
-if __name__ == "__main__":
+if __name__ == "__main__": #boiler plate, no touch!!!!
     app.run(host="localhost", debug=True, port=8080)
     

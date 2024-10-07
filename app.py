2f9eadec16e34a096044e0469c26eabf8a6bf931
joewadk <jkabir000@citymail.cuny.edu>
2024-10-06 23:46:05 -0400

    got params

--- app.py
+++ app.py
@@ -7,8 +7,33 @@ app = Flask(__name__)
 
 @app.route("/search", methods=["GET"])
 def search():
-    abort(501)
+    '''
+    Below you will find several examples of requests that clients might make:
+  - `curl --location --request GET 'http://127.0.0.1:8080/search?restaurant_name=pizza'`
+    - [Example response](golden_files/search_by_restaurant_name.json)
+  - `curl --location --request GET 'http://127.0.0.1:8080/search?zipcode=10031'`
+    - [Example response](golden_files/search_by_zipcode.json)
+  - `curl --location --request GET 'http://127.0.0.1:8080/search?cuisine=italian&limit=5'`
+    - [Example response](golden_files/search_by_cuisine_with_limit.json)
+  - `curl --location --request GET 'http://127.0.0.1:8080/search?cuisine=mexican&zipcode=10003'`
+    - [Example response](golden_files/search_by_cuisine_and_zipcode.json)
+  - `curl --location --request GET 'http://127.0.0.1:8080/search?restaurant_name=taco&cuisine=mexican&zipcode=10003'`
+    - [Example response](golden_files/search_by_restaurant_name_cuisine_and_zipcode.json)
+    ''' #adding here for self-reference
+
+    #create inspector object
+    #load json
+    #search for any args
+
+    restaurant=request.args.get('restaurant_name')
+    cuisine=request.args.get('cuisine')
+    zipcode=request.args.get('zipcode')
+    print(restaurant, cuisine, zipcode)
+    search = Inspector()
+    inspections = list(search.get_inspections())
+    return inspections.to_json()
 
 
 if __name__ == "__main__":
     app.run(host="localhost", debug=True, port=8080)
+    
